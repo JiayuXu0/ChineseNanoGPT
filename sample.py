421bd0a6853c9c3d516c9088291975b980633673
JiayuXu <13246627668@qq.com>
2024-11-16 13:17:39 +0800

    修改推理数据输入类型

--- sample.py
+++ sample.py
@@ -11,7 +11,7 @@ device = "cuda"
 torch.manual_seed(1337)
 torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
 torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
-
+dtype = torch.float16
 out_dir = "out"
 ckpt_path = os.path.join(out_dir, "ckpt.pt")
 checkpoint = torch.load(ckpt_path, map_location=device)
@@ -19,20 +19,21 @@ checkpoint = torch.load(ckpt_path, map_location=device)
 # model
 gptconf = GPTConfig(**checkpoint["model_args"])
 model = GPT(gptconf)
+model = torch.compile(model, mode="reduce-overhead")
 model.load_state_dict(checkpoint["model"])
 model.eval()
 model.to(device)
 tokenizer = BertTokenizer.from_pretrained("bert-base-chinese")
 
 # start = enc.encode("\n")
-start = tokenizer("书架上")["input_ids"][1:-1]
+start = tokenizer("北京市包括")["input_ids"][1:-1]
 print(start)
 x = torch.tensor(start, dtype=torch.long, device=device)[None, ...]
 
 for k in range(1):
 
     with torch.no_grad():
-        with torch.amp.autocast(device_type="cuda", dtype=torch.bfloat16):
+        with torch.amp.autocast(device_type="cuda", dtype=dtype):
             y = model.generate(x, 300, temperature=0.95, top_k=50)
 
     print(tokenizer.decode(y[0].tolist()))
